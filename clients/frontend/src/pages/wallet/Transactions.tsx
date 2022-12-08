07145ed3a8876cbaf9058c712e1b452cacbe6ce5
kasumisk <kasumisk@163.com>
2022-12-08 20:11:06 +0800

    transaction history.

--- clients/frontend/src/pages/wallet/Transactions.tsx
+++ clients/frontend/src/pages/wallet/Transactions.tsx
@@ -2,12 +2,13 @@ import { RootDispatch, RootState } from '@/store';
 import { hasOwnProperty } from '@/utils';
 import { PageContainer } from '@ant-design/pro-components';
 import { useConnect } from '@connect2ic/react';
-import { Button, Collapse, List, Spin } from 'antd';
+import { Button, Collapse, List, Spin, Tabs } from 'antd';
 import { useEffect } from 'react';
 import dayjs from 'dayjs';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { Action } from '../../../../idls/ms_controller';
+import { TeamOutlined } from '@ant-design/icons';
 
 const WalletTransactions = () => {
   const dispatch = useDispatch<RootDispatch>();
@@ -41,56 +42,94 @@ const WalletTransactions = () => {
 
   console.log('actions', actions);
 
+  const actionRender = (actions: Action[]) => {
+    if (actions.length === 0) return null;
+    return (
+      <Collapse defaultActiveKey={[]}>
+        {actions.map(action => (
+          <Collapse.Panel
+            header={
+              <div className="flex justify-between items-center">
+                <div className="flex">
+                  {hasOwnProperty(action.status, 'SUCCESS')
+                    ? 'Success'
+                    : hasOwnProperty(action.status, 'SINGING')
+                    ? 'Singing'
+                    : hasOwnProperty(action.status, 'INIT')
+                    ? 'init'
+                    : 'timeout'}
+                  <p className="ml-3">
+                    {dayjs(
+                      Math.ceil(Number(action.create_at) / 1000000),
+                    ).format('YYYY-MM-DD HH:mm:ss')}
+                  </p>
+                  <p className="flex ml-3">
+                    <TeamOutlined />
+                    {`${action.signs.length} out of ${activeController?.threshold_user_amount}`}
+                  </p>
+                </div>
+                <Button
+                  type="primary"
+                  disabled={
+                    !!action.signs.find(
+                      o => o.user_id.toText() === activeProvider?.principal,
+                    )
+                  }
+                  onClick={() => actionSign(action)}
+                >
+                  Sign
+                </Button>
+              </div>
+            }
+            key={action.id.toString()}
+          >
+            <List
+              dataSource={action.signs}
+              renderItem={sign => (
+                <List.Item>{sign.user_id.toText()}</List.Item>
+              )}
+            />
+          </Collapse.Panel>
+        ))}
+      </Collapse>
+    );
+  };
+
   return (
     <PageContainer ghost>
+      <Tabs></Tabs>
       {loading.models.controller ? (
         <div className="flex justify-center">
           <Spin />
         </div>
       ) : (
-        <Collapse defaultActiveKey={[]}>
-          {actions.map(action => (
-            <Collapse.Panel
-              header={
-                <div className="flex justify-between">
-                  <div className="flex">
-                    {hasOwnProperty(action.status, 'SUCCESS')
-                      ? 'Success'
-                      : hasOwnProperty(action.status, 'SINGING')
-                      ? 'Singing'
-                      : hasOwnProperty(action.status, 'INIT')
-                      ? 'init'
-                      : 'timeout'}
-                    <p className="ml-3">
-                      {dayjs(
-                        Math.ceil(Number(action.create_at) / 1000000),
-                      ).format('YYYY-MM-DD HH:mm:ss')}
-                    </p>
-                  </div>
-                  <Button
-                    type="primary"
-                    disabled={
-                      !!action.signs.find(
-                        o => o.user_id.toText() === activeProvider?.principal,
-                      )
-                    }
-                    onClick={() => actionSign(action)}
-                  >
-                    Sign
-                  </Button>
-                </div>
-              }
-              key={action.id.toString()}
-            >
-              <List
-                dataSource={action.signs}
-                renderItem={sign => (
-                  <List.Item>{sign.user_id.toText()}</List.Item>
-                )}
-              />
-            </Collapse.Panel>
-          ))}
-        </Collapse>
+        <Tabs
+          defaultActiveKey="1"
+          items={[
+            {
+              label: `Queue`,
+              key: '1',
+              children: actionRender(
+                actions.filter(
+                  action =>
+                    hasOwnProperty(action.status, 'INIT') ||
+                    hasOwnProperty(action.status, 'SINGING'),
+                ),
+              ),
+            },
+            {
+              label: `History`,
+              key: '2',
+              children: actionRender(
+                actions.filter(
+                  action =>
+                    hasOwnProperty(action.status, 'SUCCESS') ||
+                    hasOwnProperty(action.status, 'TIMEOUT'),
+                ),
+              ),
+            },
+          ]}
+        />
       )}
       {/* <List
         loading={loading.models.controller}
